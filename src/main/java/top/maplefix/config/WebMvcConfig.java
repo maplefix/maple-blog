ec37129c5638b4a93e2ce4eb8f8c99e5a327da38
maplefix <maplefix@163.com>
2020-03-13 09:15:15 +0800

    add core config

--- src/main/java/top/maplefix/config/WebMvcConfig.java
+++ src/main/java/top/maplefix/config/WebMvcConfig.java
@@ -1,33 +1,26 @@
 package top.maplefix.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import top.maplefix.interceptor.LoginInterceptor;
+
+import java.nio.file.Paths;
 
 /**
  * @author : Maple
- * @description : 自定义web拦截器
- * @date : Created in 2020/1/16 9:57
+ * @description : 自定义web拦截器,资源文件管理配置
+ * @date : 2020/1/16 9:57
  */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Bean
-    public LoginInterceptor getLoginInterceptor() {
-        return new LoginInterceptor();
-    }
-
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        //所有已api开头的访问都要进入RedisSessionInterceptor拦截器进行登录验证，并排除login接口(全路径)。必须写成链式，分别设置的话会创建多个拦截器。
-        //必须写成getSessionInterceptor()，否则SessionInterceptor中的@Autowired会无效
-        registry.addInterceptor(getLoginInterceptor()).addPathPatterns("/api/**")
-                .excludePathPatterns("/api/admin/captcha").excludePathPatterns("/api/admin/login")
-                .excludePathPatterns("/admin/dist/**").excludePathPatterns("/admin/plugins/**")
-                .excludePathPatterns("/blog/default/**").excludePathPatterns("/blog/plugins/**")
-                .excludePathPatterns("/druid/**");
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String pathUtl = Paths.get(SystemConfig.getProfile()).normalize().toUri().toASCIIString();
+        registry.addResourceHandler("/file/**").addResourceLocations(pathUtl).setCachePeriod(0);
+
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
 }
