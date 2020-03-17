92e511cf3650cab6a1035fc6ff8d07fb5462b7fa
maplefix <maplefix@163.com>
2020-03-17 20:04:21 +0800

    add redis client config

--- src/main/java/top/maplefix/config/RedisConfig.java
+++ src/main/java/top/maplefix/config/RedisConfig.java
@@ -22,7 +22,7 @@ import java.time.Duration;
 /**
  * @author : Maple
  * @description : redis相关配置
- * @date : Created in 2020/1/16 10:11
+ * @date : 2020/1/16 10:11
  */
 @Configuration
 @EnableCaching
