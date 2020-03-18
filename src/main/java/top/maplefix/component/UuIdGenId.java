14956ef18eb3560df79f352237188782b76fe775
maplefix <maplefix@163.com>
2020-03-18 09:53:40 +0800

    update UuIdGenId

--- src/main/java/top/maplefix/component/UuIdGenId.java
+++ src/main/java/top/maplefix/component/UuIdGenId.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 /**
  * @author : Maple
  * @description : 实现tk的id生成策略（tk只提供接口未实现）
- * @date : Created in 2020/1/15 14:33
+ * @date : 2020/1/15 14:33
  */
 public class UuIdGenId implements GenId<String> {
 
