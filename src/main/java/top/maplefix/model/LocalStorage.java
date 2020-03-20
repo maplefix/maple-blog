bcb14e252dcecb24a6bed0c9d87f8e06a39b1291
maplefix <maplefix@163.com>
2020-03-20 14:15:50 +0800

    add file storage cls

---
+++ src/main/java/top/maplefix/model/LocalStorage.java
@@ -0,0 +1,56 @@
+package top.maplefix.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import tk.mybatis.mapper.annotation.KeySql;
+import top.maplefix.component.UuIdGenId;
+
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * @author wangjg
+ * @description 本地存储实体
+ * @date 2020/3/18 17:21
+ */
+@Data
+@NoArgsConstructor
+public class LocalStorage extends BaseEntity implements Serializable {
+
+    @Id
+    @KeySql(genId = UuIdGenId.class)
+    private String  fileId;
+    /**
+     * 文件真实名称
+     */
+    private String realName;
+    /**
+     * 文件名
+     */
+    private String name;
+    /**
+     * 后缀
+     */
+    private String suffix;
+    /**
+     * 路径
+     */
+    private String path;
+    /**
+     * 类型
+     */
+    private String type;
+    /**
+     * 大小
+     */
+    private String size;
+
+    public LocalStorage(String realName, String name, String suffix, String path, String type, String size) {
+        this.realName = realName;
+        this.name = name;
+        this.suffix = suffix;
+        this.path = path;
+        this.type = type;
+        this.size = size;
+    }
+}
---
+++ src/main/java/top/maplefix/model/QiNiuContent.java
@@ -0,0 +1,42 @@
+package top.maplefix.model;
+
+import lombok.Data;
+
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * @author wangjg
+ * @description 七牛云文件存储
+ * @date 2020/3/20 13:44
+ */
+@Data
+public class QiNiuContent extends BaseEntity implements Serializable {
+    @Id
+    private String fileId;
+    /**
+     * 文件名
+     */
+    private String name;
+    /**
+     * 空间名
+     */
+    private String bucket;
+    /**
+     * 大小,eg:5kb
+     */
+    private String size;
+    /**
+     * 访问地址
+     */
+    private String url;
+    /**
+     * 文件后缀
+     */
+    private String suffix;
+    /**
+     * 空间类型
+     */
+    private String type = "公开";
+}
+
