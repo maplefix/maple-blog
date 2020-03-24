b217b9d891378edb78ef1e9821657408908d143a
maplefix <maplefix@163.com>
2020-03-24 14:08:40 +0800

    add qiniuyun config

--- src/main/java/top/maplefix/config/QiNiuConfig.java
+++ src/main/java/top/maplefix/config/QiNiuConfig.java
@@ -1,5 +1,6 @@
 package top.maplefix.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
@@ -7,9 +8,10 @@ import org.springframework.stereotype.Component;
 /**
  * @author : Maple
  * @description : 七牛云相关配置
- * @date : Created in 2020/1/16 9:57
+ * @date : 2020/1/16 9:57
  */
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Component
 @ConfigurationProperties(prefix = "qiniu")
 public class QiNiuConfig {
