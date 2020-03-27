1b5da08ee35679cbaf3ad0adece5e84973544bdd
maplefix <maplefix@163.com>
2020-03-27 09:35:20 +0800

    update base controller

--- src/main/java/top/maplefix/controller/BaseController.java
+++ src/main/java/top/maplefix/controller/BaseController.java
@@ -1,15 +1,28 @@
 package top.maplefix.controller;
 
+import com.github.pagehelper.PageInfo;
+import com.github.pagehelper.page.PageMethod;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import top.maplefix.constant.PageConstant;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import top.maplefix.common.BaseResult;
+import top.maplefix.utils.DateUtils;
+import top.maplefix.utils.SqlUtil;
 import top.maplefix.utils.StringUtils;
+import top.maplefix.vo.page.PageDomain;
+import top.maplefix.vo.page.TableDataInfo;
+import top.maplefix.vo.page.TableSupport;
 
 import javax.servlet.http.HttpServletRequest;
+import java.beans.PropertyEditorSupport;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 /**
  * @author : Maple
@@ -21,6 +34,60 @@ public class BaseController {
 
     @Autowired
     private MessageSource messageSource;
+
+    /**
+     * 将前台传递过来的日期格式的字符串，自动转化为Date类型
+     */
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        // Date 类型转换
+        binder.registerCustomEditor(Date.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) {
+                setValue(DateUtils.parseDate(text));
+            }
+        });
+    }
+
+
+    /**
+     * 设置请求分页数据
+     */
+    protected void startPage() {
+        PageDomain pageDomain = TableSupport.buildPageRequest();
+        Integer pageNum = pageDomain.getPageNum();
+        Integer pageSize = pageDomain.getPageSize();
+        if (StringUtils.isNotNull(pageNum) && StringUtils.isNotNull(pageSize)) {
+            String orderBy = SqlUtil.escapeOrderBySql(pageDomain.getOrderBy());
+            PageMethod.startPage(pageNum, pageSize, orderBy);
+        }
+    }
+
+    /**
+     * 响应请求分页数据
+     * @param list 列表数据
+     * @return TableDataInfo
+     */
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    protected TableDataInfo getDataTable(List<?> list) {
+        TableDataInfo rspData = new TableDataInfo();
+        rspData.setCode(HttpStatus.OK.value());
+        rspData.setRows(list);
+        if (list == null) {
+            list = new ArrayList<>();
+        }
+        rspData.setTotal(new PageInfo(list).getTotal());
+        return rspData;
+    }
+    /**
+     * 响应返回结果
+     *
+     * @param rows 影响行数
+     * @return 操作结果
+     */
+    protected BaseResult toResult(int rows) {
+        return rows > 0 ? BaseResult.success() : BaseResult.error();
+    }
 
     /**
      *获取国际化后的字符串
@@ -46,17 +113,5 @@ public class BaseController {
         }
         return returnMsg;
     }
-
-    /**
-     * 校验分页查询参数是否完整
-     * @param params 参数map
-     * @return
-     */
-    public boolean checkPageParam(Map<String, Object> params){
-        int currPage = top.maplefix.utils.StringUtils.getObjInt(params.get(PageConstant.PAGENUM));
-        int pageSize = top.maplefix.utils.StringUtils.getObjInt(params.get(PageConstant.PAGESIZE));
-        return !org.springframework.util.StringUtils.isEmpty(currPage) && !org.springframework.util.StringUtils.isEmpty(pageSize);
-    }
-
 
 }
