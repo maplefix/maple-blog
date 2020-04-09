9e0779330bbb6de1a89351a3f029d4c3884c8831
maplefix <maplefix@163.com>
2020-04-09 19:20:47 +0800

    修改验证码接口

---
+++ src/main/java/top/maplefix/controller/oms/CaptchaController.java
@@ -0,0 +1,65 @@
+package top.maplefix.controller.oms;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.maplefix.common.BaseResult;
+import top.maplefix.constant.Constant;
+import top.maplefix.redis.RedisCacheService;
+import top.maplefix.utils.Base64;
+import top.maplefix.utils.UuidUtils;
+import top.maplefix.utils.VerifyCodeUtils;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author : Maple
+ * @description : 验证码接口
+ * @date : 2020/1/24 19:42
+ */
+@RestController
+@RequestMapping("/captcha")
+@Slf4j
+public class CaptchaController {
+
+    @Autowired
+    private RedisCacheService redisCacheService;
+
+    /**
+     * 生成验证码
+     */
+    @GetMapping("/captchaImage")
+    public BaseResult getCode(HttpServletResponse response) throws IOException {
+        // 生成随机字串
+        String verifyCode = VerifyCodeUtils.generateVerifyCode(4);
+        // 唯一标识
+        String uuid = UuidUtils.getRandomUuidWithoutSeparator();
+        String verifyKey = Constant.CAPTCHA_CODE_KEY + uuid;
+
+        redisCacheService.setCacheObject(verifyKey, verifyCode, Constant.CAPTCHA_EXPIRATION, TimeUnit.MINUTES);
+        // 生成图片
+        int w = 111;
+        int h = 36;
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        VerifyCodeUtils.outputImage(w, h, stream, verifyCode);
+        try {
+            BaseResult baseResult = BaseResult.success();
+            JSONObject data = new JSONObject();
+            data.put("uuid", uuid);
+            data.put("img", Base64.encode(stream.toByteArray()));
+            baseResult.setData(data);
+            return baseResult;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return BaseResult.error(e.getMessage());
+        } finally {
+            stream.close();
+        }
+    }
+}
