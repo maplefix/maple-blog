62c6c6c2394a374628351e0c3365b3b66e04fce3
maplefix <maplefix@163.com>
2020-04-13 14:07:25 +0800

    新增服务器监控数据获取

--- src/main/java/top/maplefix/controller/oms/ServerController.java
+++ src/main/java/top/maplefix/controller/oms/ServerController.java
@@ -1,18 +1,17 @@
-package top.maplefix.controller.backend;
+package top.maplefix.controller.oms;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import top.maplefix.common.BaseResult;
 import top.maplefix.controller.BaseController;
 import top.maplefix.vo.server.Server;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author : Maple
  * @description : 服务器监控相关信息
- * @date : Created in 2019/9/15 16:20
- * @version : v1.0
+ * @date : 2020/2/15 16:20
  */
 @Controller
 @RequestMapping("/api/admin/server")
@@ -20,16 +19,14 @@ public class ServerController extends BaseController {
 
     /**
      * 服务器相关信息
-     * @param request
      * @return
      * @throws Exception
      */
-    @GetMapping({"","/"})
-    public String server(HttpServletRequest request) throws Exception {
+    @PreAuthorize("@permissionService.hasPermission('monitor:server:list')")
+    @GetMapping()
+    public BaseResult getInfo() throws Exception {
         Server server = new Server();
         server.copyTo();
-        request.setAttribute("server", server);
-        request.setAttribute("path", "server");
-        return "admin/server";
+        return BaseResult.success(server);
     }
 }
