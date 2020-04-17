4404ccad066dc4c8add967c8302a73a715caa7bc
maplefix <maplefix@163.com>
2020-04-17 13:46:59 +0800

    新增目录类型泛型

---
+++ src/main/java/top/maplefix/enums/CategoryType.java
@@ -0,0 +1,30 @@
+package top.maplefix.enums;
+
+/**
+ * @author wangjg
+ * @description 目录类型
+ * @date 2020/4/17 11:36
+ */
+public enum  CategoryType {
+    /**
+     * type of blog
+     */
+    BLOG(1),
+    /**
+     * type of note
+     */
+    NOTE(2),
+    /**
+     * type of book
+     */
+    BOOK(3);
+    private Integer type;
+
+    CategoryType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+}
