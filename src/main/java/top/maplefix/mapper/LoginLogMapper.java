6d98721c2b7bcc52c91a6e845497eb09a32e45d6
maplefix <maplefix@163.com>
2020-06-10 11:16:21 +0800

    新增操作/登陆日志记录

--- src/main/java/top/maplefix/mapper/LoginLogMapper.java
+++ src/main/java/top/maplefix/mapper/LoginLogMapper.java
@@ -1,17 +1,54 @@
 package top.maplefix.mapper;
 
-import org.apache.ibatis.annotations.CacheNamespace;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.ids.SelectByIdsMapper;
 import top.maplefix.model.LoginLog;
+
+import java.util.List;
 
 /**
  * @author : Maple
  * @description : 登录日志mapper
- * @date : 2019/7/25 0:32
- * @version : v1.0
+ * @date : 2020/2/25 0:32
  */
-@CacheNamespace
-public interface LoginLogMapper extends Mapper<LoginLog> , SelectByIdsMapper<LoginLog> {
+public interface LoginLogMapper extends Mapper<LoginLog>{
 
+    /**
+     * 新增系统登录日志
+     *
+     * @param loginLog 访问日志对象
+     */
+    void insertLoginLog(LoginLog loginLog);
+
+    /**
+     * 查询系统登录日志集合
+     *
+     * @param loginLog 访问日志对象
+     * @return 登录记录集合
+     */
+    List<LoginLog> selectLoginLogList(LoginLog loginLog);
+
+    /**
+     * 批量删除系统登录日志
+     *
+     * @param ids 需要删除的数据
+     * @return 结果
+     */
+    int deleteLoginLogByIds(@Param("ids") String[] ids);
+
+    /**
+     * f
+     * 清空系统登录日志
+     *
+     * @return 结果
+     */
+    int cleanLoginLog();
+
+    /**
+     * update login log
+     *
+     * @param loginLog login log
+     * @return update count
+     */
+    int updateLoginLog(LoginLog loginLog);
 }
--- src/main/java/top/maplefix/mapper/OperateLogMapper.java
+++ src/main/java/top/maplefix/mapper/OperateLogMapper.java
@@ -1,17 +1,59 @@
 package top.maplefix.mapper;
 
-import org.apache.ibatis.annotations.CacheNamespace;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.ids.SelectByIdsMapper;
 import top.maplefix.model.OperateLog;
+
+import java.util.List;
 
 /**
  * @author : Maple
  * @description : 操作日志mapper
- * @date : 2019/7/25 0:33
- * @version : v1.0
+ * @date : 2020/1/25 0:33 
  */
-@CacheNamespace
-public interface OperateLogMapper extends Mapper<OperateLog>, SelectByIdsMapper<OperateLog> {
+public interface OperateLogMapper extends Mapper<OperateLog>{
 
+    /**
+     * 新增操作日志
+     *
+     * @param operLog 操作日志对象
+     */
+    void insertOperateLog(OperateLog operLog);
+
+    /**
+     * 查询系统操作日志集合
+     *
+     * @param operLog 操作日志对象
+     * @return 操作日志集合
+     */
+    List<OperateLog> selectOperateLogList(OperateLog operLog);
+
+    /**
+     * 批量删除系统操作日志
+     *
+     * @param ids      需要删除的数据
+     * @return 结果
+     */
+    int deleteOperateLogByIds(@Param("ids") String[] ids);
+
+    /**
+     * 查询操作日志详细
+     *
+     * @param id 操作ID
+     * @return 操作日志对象
+     */
+    OperateLog selectOperateLogById(@Param("id")String id);
+
+    /**
+     * 清空操作日志
+     */
+    void cleanOperateLog();
+
+    /**
+     * update operate log
+     *
+     * @param operateLog operateLog
+     * @return update count
+     */
+    int updateOperateLog(OperateLog operateLog);
 }
