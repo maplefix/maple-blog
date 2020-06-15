9e8c17faf011cecc4400fdedbca622233fe0892f
maplefix <maplefix@163.com>
2020-06-15 15:28:48 +0800

    新增用户查询

--- src/main/java/top/maplefix/mapper/UserMapper.java
+++ src/main/java/top/maplefix/mapper/UserMapper.java
@@ -1,16 +1,96 @@
 package top.maplefix.mapper;
 
-import org.apache.ibatis.annotations.CacheNamespace;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
-import top.maplefix.model.User;
+import top.maplefix.model.SysUser;
+
+import java.util.List;
 
 /**
  * @author : Maple
  * @description : 用户mapper
- * @date : 2019/7/25 0:33
- * @version : v1.0
+ * @date : 2020/1/25 0:33 
  */
-@CacheNamespace
-public interface UserMapper extends Mapper<User> {
+public interface UserMapper extends Mapper<SysUser> {
 
+    /**
+     * 根据条件分页查询用户列表
+     *
+     * @param sysUser 用户信息
+     * @return 用户信息集合信息
+     */
+    List<SysUser> selectUserList(SysUser sysUser);
+
+    /**
+     * 通过用户名查询用户
+     *
+     * @param userName 用户名
+     * @return 用户对象信息
+     */
+    SysUser selectUserByUserName(String userName);
+
+    /**
+     * 通过用户ID查询用户
+     *
+     * @param userId 用户ID
+     * @return 用户对象信息
+     */
+    SysUser selectUserById(String userId);
+
+    /**
+     * 新增用户信息
+     *
+     * @param user 用户信息
+     * @return 结果
+     */
+    int insertUser(SysUser user);
+
+    /**
+     * 修改用户信息
+     *
+     * @param user 用户信息
+     * @return 结果
+     */
+    int updateUser(SysUser user);
+
+    /**
+     * 重置用户密码
+     *
+     * @param userName 用户名
+     * @param password 密码
+     * @return 结果
+     */
+    int resetUserPwd(@Param("userName") String userName, @Param("password") String password);
+
+    /**
+     * 通过用户ID删除用户
+     *
+     * @param ids 用户ID
+     * @return 结果
+     */
+    int deleteUserByIds(@Param("ids") String[] ids, @Param("loginUsername") String loginUsername);
+
+    /**
+     * 校验用户名称是否唯一
+     *
+     * @param userName 用户名称
+     * @return 结果
+     */
+    int checkUserNameUnique(String userName);
+
+    /**
+     * 校验手机号码是否唯一
+     *
+     * @param phone 手机号码
+     * @return 结果
+     */
+    SysUser checkPhoneUnique(String phone);
+
+    /**
+     * 校验email是否唯一
+     *
+     * @param email 用户邮箱
+     * @return 结果
+     */
+    SysUser checkEmailUnique(String email);
 }
