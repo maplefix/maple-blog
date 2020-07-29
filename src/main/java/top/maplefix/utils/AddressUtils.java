611d7becd8629a33c7d9adb2cb267b9807eefc81
maplefix <maplefix@163.com>
2020-07-29 17:28:39 +0800

    新增ip2region解析IP地址

--- src/main/java/top/maplefix/utils/AddressUtils.java
+++ src/main/java/top/maplefix/utils/AddressUtils.java
@@ -2,7 +2,15 @@ package top.maplefix.utils;
 
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
+import org.lionsoul.ip2region.DataBlock;
+import org.lionsoul.ip2region.DbConfig;
+import org.lionsoul.ip2region.DbSearcher;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.client.RestTemplate;
+import top.maplefix.utils.file.FileUtils;
+
+import java.io.File;
+import java.lang.reflect.Method;
 
 /**
  * @author : Maple
@@ -122,6 +130,34 @@ public class AddressUtils {
         long end = System.currentTimeMillis();
         log.info("腾讯位置API耗时：{}", (end-start));
         return address;
+    }
+
+    /**
+     * 根据Ip使用ip2region进行查询地址
+     *
+     * @param ip ip地址
+     * @return 地址信息
+     */
+    public static String getCityInfoByIp(String ip) {
+        try {
+            String path = "ip2region/ip2region.db";
+            String name = "ip2region.db";
+            DbConfig config = new DbConfig();
+            File file = FileUtils.inputStreamToFile(new ClassPathResource(path).getInputStream(), name);
+            DbSearcher searcher = new DbSearcher(config, file.getPath());
+            Method method;
+            method = searcher.getClass().getMethod("btreeSearch", String.class);
+            DataBlock dataBlock;
+            dataBlock = (DataBlock) method.invoke(searcher, ip);
+            String address = dataBlock.getRegion().replace("0|", "");
+            if (address.charAt(address.length() - 1) == '|') {
+                address = address.substring(0, address.length() - 1);
+            }
+            return address.equals("内网IP|内网IP") ? "内网IP" : address;
+        } catch (Exception e) {
+            log.error("Get Error In Get City Address ,{}", e.getMessage(), e);
+        }
+        return "";
     }
 
     public static void main(String[] args) {
