d202d81fb6e37a686fe29adf1d6bd059fcd0d73b
maplefix <maplefix@163.com>
2020-07-30 11:26:26 +0800

    新增字段校验

--- src/main/java/top/maplefix/model/Link.java
+++ src/main/java/top/maplefix/model/Link.java
@@ -2,12 +2,15 @@ package top.maplefix.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
 import tk.mybatis.mapper.annotation.KeySql;
 import top.maplefix.annotation.Excel;
 import top.maplefix.component.UuIdGenId;
 
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 import java.io.Serializable;
 
 /**
@@ -15,8 +18,8 @@ import java.io.Serializable;
  * @description : 友链实体
  * @date : 2020/1/15 14：51
  */
-@EqualsAndHashCode(callSuper = true)
 @Data
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Link extends BaseEntity implements Serializable {
 
@@ -31,27 +34,32 @@ public class Link extends BaseEntity implements Serializable {
      * 友链名称
      */
     @Excel(name = "友链名称")
+    @Length(min = 3, max = 50, message = "名称长度为{min}~{max}")
     private String linkName;
     /**
      * 链接地址
      */
     @Excel(name = "链接地址")
+    @URL(message = "请输入正确的Url地址")
     private String url;
     /**
      * 描述
      */
     @Excel(name = "描述")
+    @Length(min = 3, max = 500, message = "友链描述长度为{min}~{max}")
     private String description;
     /**
-     * 头像地址
+     * 网站图片
      */
-    @Excel(name = "头像地址")
+    @Excel(name = "网站图片")
+    @URL(message = "请输入正确的网站图片地址")
     private String headerImg;
 
     /**
      * 邮箱
      */
     @Excel(name = "邮箱")
+    @Email(message = "请输入正确的站长邮箱地址")
     private String email;
     /**
      * 是否显示(1:显示,0:不显示)
